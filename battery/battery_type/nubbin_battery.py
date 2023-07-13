3dbf64fc0afdab06d008e6076f4c95fd694d1d6e
Jiayi Nie <nie.jiay@northeastern.edu>
2023-07-13 16:44:06 -0700

    update spindler battery's year to be serviced

--- battery/battery_type/nubbin_battery.py
+++ battery/battery_type/nubbin_battery.py
@@ -5,10 +5,11 @@ class NubbinBattery(Battery):
     def __init__(self, last_service_date, current_date):
         self.last_service_date = last_service_date
         self.current_date = current_date
+        self.years_to_be_service = 4
 
     def needs_service(self):
         service_threshold_date = self.last_service_date.replace(
-            year=self.last_service_date.year + 4)
+            year=self.last_service_date.year + self.years_to_be_service)
         if service_threshold_date < self.current_date:
             return True
         else:
--- test/test_battery.py
+++ test/test_battery.py
@@ -21,7 +21,7 @@ class TestNubbinBattery(unittest.TestCase):
 class TestSpindlerBattery(unittest.TestCase):
     def test_battery_should_be_serviced(self):
         current_date = datetime.today().date()
-        last_service_date = current_date.replace(year=current_date.year - 3)
+        last_service_date = current_date.replace(year=current_date.year - 4)
         battery = SpindlerBattery(last_service_date, current_date)
         self.assertTrue(battery.needs_service())
 
